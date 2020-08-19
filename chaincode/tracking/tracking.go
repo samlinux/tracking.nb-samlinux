385535dde0474dfe69bfafbfb2cbebf9a336ae57
Roland Bole <rbole@samlinux.at>
2020-08-19 14:49:44 +0200

    add chaincode tracking

---
+++ chaincode/tracking/tracking.go
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2020 by rbole@samlinux.at
+ */
+
+package main
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+// TrackingAsset implements a simple chaincode to manage an asset
+type TrackingAsset struct {
+}
+
+type Packet struct {
+	Owner string `json:"owner,omitempty"`
+}
+
+// main function starts up the chaincode in the container during instantiate
+func main() {
+	if err := shim.Start(new(TrackingAsset)); err != nil {
+		fmt.Printf("Error starting TrackingAsset chaincode: %s", err)
+	}
+}
+
+// Init is called during chaincode instantiation to initialize any
+// data. Note that chaincode upgrade also calls this function to reset
+// or to migrate data.
+func (t *TrackingAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+// Invoke is called per transaction on the chaincode.
+func (t *TrackingAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+	// Extract the function and args from the transaction proposal
+	fn, args := stub.GetFunctionAndParameters()
+
+	if fn == "set" {
+		return t.storeTracking(stub, args)
+	} else if fn == "history" {
+		return t.getHistory(stub, args)
+	}
+
+	// if no case match an error will be thrown
+	return shim.Error("Invalid invoke function name. Expecting \"set\" \"history\" ")
+}
+
+// Set stores the asset (both key and value) on the ledger.
+// If the key exists, it will override the value with the new one
+func (t *TrackingAsset) storeTracking(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	var err error
+	if len(args) != 1 {
+		fmt.Println("Incorrect arguments. Expecting a key and a value")
+	}
+
+	certificate, _ := stub.GetCreator()
+	commonName := getIdentityCommonName(certificate)
+	fmt.Printf("CommonName: %+s\n", commonName)
+
+	// create the new packet object
+	packet := &Packet{commonName}
+
+	fmt.Printf("Packet: %+v\n", packet)
+	packetJSONasBytes, err := json.Marshal(packet)
+	if err != nil {
+		fmt.Println("Failed to parse data")
+	}
+
+	err = stub.PutState(args[0], packetJSONasBytes)
+	if err != nil {
+		fmt.Printf("Failed to set asset: %s\n", args[0])
+	}
+
+	return shim.Success(nil)
+}
+
+// getHistory get all transactions of a given packet ID
+func (t *TrackingAsset) getHistory(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+	packetId := args[0]
+
+	fmt.Printf("- start getHistoryForPacket: %s\n", packetId)
+
+	resultsIterator, err := stub.GetHistoryForKey(packetId)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer resultsIterator.Close()
+
+	// buffer is a JSON array containing historic values for the marble
+	var buffer bytes.Buffer
+	buffer.WriteString("[")
+
+	bArrayMemberAlreadyWritten := false
+	for resultsIterator.HasNext() {
+		response, err := resultsIterator.Next()
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+		// Add a comma before array members, suppress it for the first array member
+		if bArrayMemberAlreadyWritten == true {
+			buffer.WriteString(",")
+		}
+		buffer.WriteString("{\"TxId\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(response.TxId)
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"Packet\":")
+
+		if response.IsDelete {
+			buffer.WriteString("null")
+		} else {
+			buffer.WriteString(string(response.Value))
+		}
+
+		buffer.WriteString(", \"Timestamp\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(time.Unix(response.Timestamp.Seconds, int64(response.Timestamp.Nanos)).String())
+		buffer.WriteString("\"")
+
+		buffer.WriteString(", \"IsDelete\":")
+		buffer.WriteString("\"")
+		buffer.WriteString(strconv.FormatBool(response.IsDelete))
+		buffer.WriteString("\"")
+
+		buffer.WriteString("}")
+		bArrayMemberAlreadyWritten = true
+	}
+	buffer.WriteString("]")
+
+	fmt.Printf("- getHistoryForPacket returning:\n%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
+}
+
+// getIdentityCommonName extracts the CommonName from the
+// transaction proposal certificate
+func getIdentityCommonName(certificate []byte) string {
+	data := certificate[strings.Index(string(certificate), "-----") : strings.LastIndex(string(certificate), "-----")+5]
+	block, _ := pem.Decode([]byte(data))
+	cert, _ := x509.ParseCertificate(block.Bytes)
+
+	//fmt.Printf("%+v\n", cert.Subject.ToRDNSequence())
+	//fmt.Printf("%s\n", cert.Subject.CommonName)
+	//fmt.Printf("%s\n", cert.Subject.SerialNumber)
+
+	// organization := cert.Issuer.Organization[0]
+	// fmt.Printf("Org: %+v\n", organization)
+	return cert.Subject.CommonName
+}
--- helper.md
+++ helper.md
@@ -1,19 +1,87 @@
 # Helper 
 
-## Preparation
+## Chaincode-dev-helper
+
+Open Terminal 1.
 ```bash
+docker-compose -f docker-compose-simple.yaml up
+```
+
+Open terminal 2.
+```bash
+docker exec -it chaincode bash
+cd tracking
+go build
+CORE_PEER_ADDRESS=peer:7052 CORE_CHAINCODE_ID_NAME=mycc:0 ./tracking
+```
+
+Open terminal 3.
+```bash
+docker exec -it cli bash
+cd /opt/gopath/src
+peer chaincode install -p chaincodedev/chaincode/tracking -n mycc -v 0
+peer chaincode instantiate -n mycc -v 0 -c '{"Args":[]}' -C myc
+
+peer chaincode invoke -n mycc -c '{"Args":["set", "2"]}' -C myc
+peer chaincode query -n mycc -c '{"Args":["history","1"]}' -C myc
+```
+
+## Network start preparation
+
+Create crypto material.
+```bash
+init.sh
+```
+
+Start the network.
+```bash
+# fabric-ca adjustment get CA PK
+ll crypto-config/peerOrganizations/tracking.nb-samlinux.com/ca/*_sk
+
+# add fabric-ca PK to docker-compose file and start the network
+docker-compose up -d
+``` 
+
+Enroll user identies.
+```bash
+
 # first make sure you have an admin, if not enroll it
 export FABRIC_CA_CLIENT_HOME=./crypto-config/peerOrganizations/tracking.nb-samlinux.com/ca/
 fabric-ca-client enroll -d -u http://root:morgen@0.0.0.0:7054 
 
 # register and enroll user identities
-./enrollUser.sh post_office_hq-tracking.nb-samlinux.com
-./enrollUser.sh post_post-tracking.nb-samlinux.com
-./enrollUser.sh xxx
+./enrollUser.sh post_office
+./enrollUser.sh post_box
 
+tree ./crypto-config/peerOrganizations/tracking.nb-samlinux.com/users/
+```
+## Logging
+```bash
+# do some logging if needed
+docker logs ca.tracking.nb-samlinux.com 2>&1 
 ```
 
-## Test
+## Stop the network
+Network is persistent.
+```bash
+docker-compose down
+```
+
+## Start the network
+Network is persistent.
+```bash
+docker-compose up -d
+```
+
+## Clean up the network
+
+```bash
+docker-compose down
+docker volume prune
+docker rm $(docker ps -a -f status=exited -q)
+```
+
+## Create channel and install chaincode
 ```bash
 
 # exec the cli container
@@ -29,13 +97,39 @@ peer channel create -o orderer.nb-samlinux.com:7050 -c $CHANNEL_NAME -f ./channe
 # join channel peer0
 peer channel join -b $CHANNEL_NAME.block
 
-# query and invoke some transactions
-peer chaincode query -n mycc -c '{"Args":["query","msg1"]}' -C $CHANNEL_NAME 
-peer chaincode invoke -n mycc -c '{"Args":["set","msg2","sunshine.com"]}' -C $CHANNEL_NAME 
+export CORE_PEER_ADDRESS="peer1.tracking.nb-samlinux.com:8051"
+peer channel join -b $CHANNEL_NAME.block
+export CORE_PEER_ADDRESS="peer0.tracking.nb-samlinux.com:7051"
 
-# test with another user
-export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/producer.sunshine.com/users/pos1-producer.sunshine.com/msp
+# install and instantiate chaincode on peer0
+peer chaincode install -n tracking -v 1.0 -p github.com/chaincode/tracking/
+peer chaincode instantiate -n tracking -v 1.0 -c '{"Args":[]}' -C tracking
+
+# install chaincode on peer1
+export CORE_PEER_ADDRESS="peer1.tracking.nb-samlinux.com:8051"
+peer chaincode install -n tracking -v 1.0 -p github.com/chaincode/tracking/
+```
+
+## Query and invoke some transactions
+```bash
+export CORE_PEER_ADDRESS="peer0.tracking.nb-samlinux.com:7051"
+peer chaincode invoke -n tracking -c '{"Args":["set","1"]}' -C $CHANNEL_NAME 
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/tracking.nb-samlinux.com/users/post_office/msp
+peer chaincode invoke -n tracking -c '{"Args":["set","1"]}' -C $CHANNEL_NAME 
+
+peer chaincode query -n tracking -c '{"Args":["history","1"]}' -C $CHANNEL_NAME | jq '.'
 
 ```
 
-docker logs ca.tracking.nb-samlinux.com 2>&1 
+## Available user
+```bash
+tree -L 1 crypto-config/peerOrganizations/tracking.nb-samlinux.com/users/
+
+crypto-config/peerOrganizations/tracking.nb-samlinux.com/users/
+├── Admin@tracking.nb-samlinux.com
+├── User1@tracking.nb-samlinux.com
+├── post_box
+└── post_office
+```
+
