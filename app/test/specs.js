6fbad118506e1518a2455f43d692d274f48b5b1e
root <rbole@samlinux.at>
2020-09-01 15:41:33 +0200

    add more tests

--- app/test/specs.js
+++ app/test/specs.js
@@ -5,24 +5,24 @@ const supertest = require('supertest');
 const util = require('util');
 const api = supertest('localhost:3000');
 
+async function doTest1(maxInt){
+  let key = getRandomInt(maxInt);
+  console.log('key: ',key)
+  let result = await api.get('/getHistory/'+key)
+  let tx = JSON.parse(result.body.value);
+  console.log(util.inspect(tx,false, null, true));
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-describe("tracking.nb.samlinux.com API tests", function() {
-  it("checks if api is running", async function() {
-    this.skip();
-    let result = await api.get('/')
-    console.log(result.body)
-  }) 
-  
-  it("create asset", async function() {
-    //this.skip();
-    let key = getRandomInt(1000000);
+async function doTest2(maxInt){
+  let key = getRandomInt(maxInt);
     key = key.toString();
-    key = '30353'
+    //key = '30353'
     let actor = 'post_office';
-    
+    console.log('send PI ',key)
     let payload = { 
       data: {
         key: key,
@@ -31,19 +31,41 @@ describe("tracking.nb.samlinux.com API tests", function() {
     }
 
     let result = await api.post('/store').send(payload)
-    console.log(result.body);
-   
+    //console.log(result.body);
+}
+
+describe("tracking.nb.samlinux.com API tests", function() {
+  it("checks if api is running", async function() {
+    //this.skip();
+    let result = await api.get('/')
+    console.log(result.body)
+  }) 
+  
+  it("create asset", async function() {
+    this.skip();
+    let maxInt = 1000000;
+    setInterval(doTest2, 100, maxInt);
   })
 
   it("get getHistory", async function() {
     this.skip();
-    let key = '356092';
-
+    /*
+    let key = '100' ;
     let result = await api.get('/getHistory/'+key)
 
     let tx = JSON.parse(result.body.value);
     console.log(util.inspect(tx,false, null, true));
-   /*
+     */ 
+    // 1500 milliseconds = 1,5 seconds
+    // 200 ms = 5 tps
+    // 100 ms = 10 tps
+    // 10 ms = 100 tps
+    // 20 ms = 50 tps
+    // 30 ms = 33 tps
+    setInterval(doTest1, 30, 10);
+    
+   
+    /*
     tx.forEach(tx => {
        console.log(util.format('TxId: %s',tx.TxId)) 
     });
