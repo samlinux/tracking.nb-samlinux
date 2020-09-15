2bf71e20f588c85bed1fb15cffc5f43358b64684
Oswin Striessnig <oswin.striessnig@samlinux.at>
2020-09-15 14:27:09 +0200

    Anpassung der Router-Paths auf 'app2'

--- frontend2_src/src/router.js
+++ frontend2_src/src/router.js
@@ -2,21 +2,15 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 const History = () => import('./components/history/History.vue');
-const Store = () => import('./components/store/Store.vue');
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/history',
+      path: '/app2/history',
       name: 'history',
       component: History,
-    },
-    {
-      path: '/store',
-      name: 'store',
-      component: Store,
     },
     {
       path: '*',
